974ee6bbf329139c65c0f709c98217f9dd2e4c75
John Rios <johnf91@protonmail.com>
2024-04-01 14:24:43 +0200

    added client react

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Class room</h1>
+      <p className='text-red-400'>this is a test por a web app with django backend</p>
+    </>
+  )
+}
+
+export default App
